263d27befd0eaf6dcd7035a39c7f5e7e2c6e53f0
Misael <misael865.nic@gmail.com>
2024-07-20 10:14:42 -0300

    array 02

---
+++ 6.js
@@ -0,0 +1,6 @@
+const elementos = [5, 10, 15, 20]
+elementos.pop()
+
+console.log(`o array com indice removido é ${elementos}`)
+
+//array00
--- array2.js
+++
@@ -1,4 +0,0 @@
-const elementos = [5, 10, 15, 20]
-elementos.pop()
-
-console.log(`o array com indice removido é ${elementos}`)
