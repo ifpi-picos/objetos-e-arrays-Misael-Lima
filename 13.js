576fa407aafde716fcc49fbd6a434266eb0f5a58
Misael <misael865.nic@gmail.com>
2024-07-23 15:41:28 -0300

    array 09

---
+++ 13.js
@@ -0,0 +1,13 @@
+let filmes = []
+
+var filme1 = prompt("Digite o nome de um filme: ")
+var filme2 = prompt("Digite outro de um filme: ")
+var filme3 = prompt("Digite outro de um filme: ")
+var filme4 = prompt("Digite outro de um filme: ")
+var filme5 = prompt("Digite outro de um filme: ")
+
+let todos=(`\n${filme1}\n${filme2}\n${filme3}\n${filme4}\n${filme5}`)
+
+filmes.push(todos)
+
+console.log(`Os filmes listados foram: ${filmes}`)
