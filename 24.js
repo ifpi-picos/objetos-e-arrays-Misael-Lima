916014e020a365eaa164dcccd8433a8221f5fc21
Misael <misael865.nic@gmail.com>
2024-07-26 14:50:20 -0300

    string 04

---
+++ 24.js
@@ -0,0 +1,6 @@
+//Crie uma string com uma frase e use o método `toUpperCase` para imprimir a frase em letras maiúsculas.
+
+const nome = ('Misael Lima')
+const nomeMaiusculo = nome.toUpperCase()
+
+console.log(nomeMaiusculo)
