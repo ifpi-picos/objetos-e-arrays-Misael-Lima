1e526daeff735a83d5b86de6186c86fc3f4fb533
Misael <misael865.nic@gmail.com>
2024-07-26 14:46:39 -0300

    string 03

--- 23.js
+++ 23.js
@@ -1,3 +1,6 @@
 //Crie uma função que recebe um nome completo do usuário e imprime somente o primeiro nome.
 
-const 
+const nomeCompleto = ('Misael De Oliveira Lima Veloso')
+const nome = nomeCompleto.split(' ')
+
+console.log(nome[0])
