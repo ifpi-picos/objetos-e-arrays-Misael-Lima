409744391a668c67316b5c9def4ee356db35ceed
Misael <misael865.nic@gmail.com>
2024-07-26 14:39:23 -0300

    string 02

---
+++ 22.js
@@ -0,0 +1,5 @@
+//Crie uma função que recebe um nome completo do usuário e imprime o primeiro e último nome.
+const nomeCompleto = ('Misael de Oliveira Lima Veloso');
+const nomes = nomeCompleto.split(' ');
+
+console.log(nomes[0]);
